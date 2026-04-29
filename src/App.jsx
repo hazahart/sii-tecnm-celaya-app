751773bbfd853fa81e27003e2df9a739d5a93c77
Gustavo Ramirez Mireles <gusramireles@hotmail.com>
2026-04-28 20:06:02 -0600

    feat: add protected route

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
+import ProtectedRoute from './components/ProtectedRoute'
 import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
+import Kardex from './pages/Kardex'
+import Horario from './pages/Horario'
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
+    return (
+        <Routes>
+            <Route path="/login" element={<Login />} />
 
-      <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/dashboard" element={
+                <ProtectedRoute><Dashboard /></ProtectedRoute>
+            } />
+            <Route path="/kardex" element={
+                <ProtectedRoute><Kardex /></ProtectedRoute>
+            } />
+            <Route path="/horario" element={
+                <ProtectedRoute><Horario /></ProtectedRoute>
+            } />
 
-      <Route path="*" element={<Navigate to="/login" replace />} />
-    </Routes>
-  )
+            <Route path="*" element={<Navigate to="/login" replace />} />
+        </Routes>
+    )
 }
 
 export default App
